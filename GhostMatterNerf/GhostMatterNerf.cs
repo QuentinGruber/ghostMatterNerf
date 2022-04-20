bbd72caa73246940ce145793e327f9d14f0c0e8b
Quentin Gruber <quentingruber@gmail.com>
2022-04-20 23:20:52 +0200

    first commit

---
+++ GhostMatterNerf/GhostMatterNerf.cs
@@ -0,0 +1,17 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using System.Collections;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace GhostMatterNerf
+{
+    public class GhostMatterNerf : ModBehaviour
+    {
+        private void Update()
+        {
+            
+         Locator.GetPlayerController()._hazardDetector._darkMatterDamagePerSecond = 1;        
+        }
+    }
+}
